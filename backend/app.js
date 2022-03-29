4fae4e0c3b9f18f0f4767b40fbeefb0f78d2bebe
Shivang-X <19it.shivangpatel@adit.ac.in>
2022-03-29 14:25:07 +0530

    *

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(fileUpload());
 app.use(cors({ origin: [
-    "http://localhost:3000",
+    // "http://localhost:3000",
     "https://spy-buy.herokuapp.com/"
     
 ], 

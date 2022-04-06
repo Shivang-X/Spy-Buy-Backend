c03343dec4a18c0b12bf2248a152540dcc520ba5
Shivang Patel <86409868+Shivang-X@users.noreply.github.com>
2022-04-06 15:50:57 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -10,7 +10,7 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-//         sameSite: false
+        samesite: none
     }
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.status(statusCode).cookie("token", token, options).json({

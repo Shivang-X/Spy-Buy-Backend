727a188db9ba5818b8a358553c32716716ea9476
Shivang-X <19it.shivangpatel@adit.ac.in>
2022-12-10 16:46:08 +0530

    Updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -159,7 +159,7 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 // Get currently logged in user details   =>   /api/v1/me
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
   const user = await User.findById(req.user.id);
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Origin', 'https://spy-buy-frontend.vercel.app/')
   res.status(200).json({
     success: true,
     user,

fd163f88192d02a5c6654b3680e97c97fedf9840
Shivang-X <19it.shivangpatel@adit.ac.in>
2022-12-10 16:31:05 +0530

    Updated

--- backend/app.js
+++ backend/app.js
@@ -18,18 +18,18 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(fileUpload());
-// app.use(cors({ origin: [
-//     "https://spy-buy.herokuapp.com",
-//     "http://localhost:3000",
-//     "https://stalwart-babka-3229ff.netlify.app/",
-//     "https://spy-buy-frontend-hefq1v7ce-shivang27102001-gmailcom.vercel.app/"
-        "https://spy-buy-frontend.vercel.app/"
+app.use(cors({ origin: [
+    "https://spy-buy.herokuapp.com",
+    "http://localhost:3000",
+    "https://stalwart-babka-3229ff.netlify.app/",
+    "https://spy-buy-frontend-hefq1v7ce-shivang27102001-gmailcom.vercel.app/",
+    "https://spy-buy-frontend.vercel.app/"
     
-// ], 
-//               credentials:true,
-//               optionSuccessStatus: 200,
-// }));
-app.use(cors())
+], 
+              credentials:true,
+              optionSuccessStatus: 200,
+}));
+// app.use(cors())
 
 
 // Import all routes
